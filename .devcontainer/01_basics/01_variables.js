e4389b9fdd3421031a509870e4cf976b83a6a3f6
Sahil1868 <sahilbisht1933@gmail.com>
2024-08-26 08:27:41 +0000

    container config done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const account_Id=144553
+let accountEmail="sahilbisht1933@google.com"
+var accountPassword="12344"
+accountCity="Bhimtal"
+let accountState="uttarakhand"
+//account_Id=2//not allowed
+
+accountEmail="hsbh@.com"
+accountPassword="25866"
+accountCity="haldwani"
+
+console.log(account_Id);
+
+console.table([account_Id,accountEmail,accountPassword,accountCity,accountState])
