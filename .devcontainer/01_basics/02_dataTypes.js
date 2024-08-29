7ef6a132d8b7c90fe5fd504a89466d458a1affd7
Sahil1868 <sahilbisht1933@gmail.com>
2024-08-29 04:28:00 +0000

    container config done

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+
+"use strict";// treat all JS code aas newer version
+
+//alert (3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+    console.log("Sahil")
+
+let name ="Sahil"
+let age = 18
+let isloggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigimt
+// string=>""
+// boolean => true/false
+// null = standalone value
+// undefined =>
+// symbol => unique
+
+
